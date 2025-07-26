713a47e86b179eaa7d76268caf47912aaf6fa322
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-26 12:37:05 -0500

    enviar notifiaciones desde postman

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:fcmappg12/firebase_options.dart';
 import 'package:fcmappg12/home_page.dart';
+import 'package:fcmappg12/utils/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.initMessaging();
+  // await NotificationService().suscribeToTopic("beneficiados");
+  await NotificationService().unsuscrbineSuscription("beneficiados");
   runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
 }
--- lib/utils/notification_service.dart
+++ lib/utils/notification_service.dart
@@ -1,1 +1,55 @@
-class NotificationService {}
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationService {
+  static FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+
+  static initMessaging() async {
+    String token = await firebaseMessaging.getToken() ?? "-";
+    print(token);
+
+    FirebaseMessaging.onMessage.listen(_onMessage);
+    FirebaseMessaging.onBackgroundMessage(_onBackGroundMessage);
+  }
+
+  //SUSCRIBIR A UN TOPIC
+  Future<void> suscribeToTopic(String topic) async {
+    await firebaseMessaging.subscribeToTopic(topic);
+    print("Dispositivo suscrito a el topic: $topic");
+  }
+
+  // CANCELAR SUSCRIPCIÓN
+  Future<void> unsuscrbineSuscription(String topic) async {
+    await firebaseMessaging.unsubscribeFromTopic(topic);
+    print("Dispositivo eliminado del topic: $topic");
+  }
+
+  // Cuando el app esta abierta
+  static _onMessage(RemoteMessage message) {
+    if (message.notification != null) {
+      print("****************************");
+      print(message.notification);
+      print(message.notification!.title);
+      print(message.notification!.body);
+      print("****************************");
+    }
+
+    if (message.data.isNotEmpty) {
+      print("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+      print("DATOS: ${message.data}");
+      print("EXTRA_DATA: ${message.data["extra_data"]}");
+      print("CÓDIGO PROMOCIONAL: ${message.data["prom_code"]}");
+      print("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+    }
+  }
+
+  static Future<void> _onBackGroundMessage(RemoteMessage message) async {
+    print("aaaa");
+    if (message.notification != null) {
+      print("------------------------------");
+      print(message.notification);
+      print(message.notification!.title);
+      print(message.notification!.body);
+      print("------------------------------");
+    }
+  }
+}
